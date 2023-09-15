5831f693483ba128f75aa48ae9f9dae4ebc1826e
Ali Forooghi <122186449+FlockOfBird@users.noreply.github.com>
2023-09-15 13:20:23 -0400

    Update main.py

--- Get Embedding/main.py
+++ Get Embedding/main.py
@@ -10,7 +10,7 @@ options for models_llama:
 '''
 models_llama = ["llama-7B", "llama-13B", "llama-30B",
                 "llama-65B", "llama2-7B", "llama2-13B", "llama2-70B"]
-
+models_bert = ["bert"]
 '''
 options for bio datasets:
     "hiv", "bace", "clintox"
@@ -19,3 +19,5 @@ options for bio datasets:
 bio_datasets = ["hiv", "bace", "clintox"]
 for model in models_llama:
     llama_base.Llama_Embeddings(model, bio_datasets)
+for model in models_bert:
+    llama_base.Bert_Embeddings(model, bio_datasets)
